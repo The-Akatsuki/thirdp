bffc2dfc16ce18ed19dec17fbede8adc00323c3c
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-08-16 19:40:24 +0530

    Payment Issues Resolved

--- src/payments/views.py
+++ src/payments/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.views import generic
+from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from . import forms
+from . import models
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
@@ -19,7 +22,7 @@ class addPayment(LoginRequiredMixin, generic.TemplateView):
 	def post(self, request, *args, **kwargs):
 		user = self.request.user
 		paymentForm = forms.paymentForm(request.POST)
-		payment = paymentForm.save(commit=false)
+		payment = paymentForm.save(commit=False)
 		payment.user = user
 		payment.save()
 		messages.success(request, "Payments has been saved!")
@@ -29,3 +32,11 @@ class addPayment(LoginRequiredMixin, generic.TemplateView):
 class paymentMethods(LoginRequiredMixin, generic.TemplateView):
 	template_name = "payments/listCreditCard.html"
 	http_method_names = ['get', 'post']
+
+	def get(self, request, *args, **kwargs):
+		user = self.request.user
+		b = models.paymentsDetails.objects.filter(user=user)
+		print b
+		kwargs["payments"] = b
+		return super(paymentMethods, self).get(request, *args, **kwargs)
+
