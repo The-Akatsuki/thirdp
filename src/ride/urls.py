76035df764459b61ffe4fe7edfa648ece01512c6
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-09-18 14:49:56 +0530

    Payment Updates

--- src/ride/urls.py
+++ src/ride/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^book$', views.BookRide.as_view(), name='book'),
     url(r'^bookaride$', views.BookRide.as_view(), name='bookaride'),
+    url(r'^bookaridereque$', views.BookRideReq.as_view(), name='bookaridereque'),
     url(r'^cancelRide$', views.cancelRide.as_view(), name='cancelRide'),
 ]
