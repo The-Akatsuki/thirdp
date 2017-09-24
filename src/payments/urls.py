18bdc9ef151e43f4a03e01386aa32b2b20f93d16
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-09-25 01:19:18 +0530

    Payment Add new Updates

--- src/payments/urls.py
+++ src/payments/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
      url(r'^(?P<id>[0-9A-Za-z_\-]+)/editpayment$', views.addPayment.as_view(), name='editpayment'),
      url(r'^(?P<id>[0-9A-Za-z_\-]+)/deletepayment$', views.deletePayment.as_view(), name='deletepayment'),
      url(r'^addpaymentjson$', views.addPaymentJSON.as_view(), name='addpaymentjson'),
+     url(r'^addpaymentrevert$', views.addPaymentrevert.as_view(), name='addpaymentrevert'),
      url(r'^paymentmethods$', views.paymentMethods.as_view(), name='paymentmethods'),
    ]
