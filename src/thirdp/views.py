93815739c01af30cfdeeef555b2c0c114c7010d4
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-09-25 09:33:32 +0530

    Payment Updates

--- src/thirdp/views.py
+++ src/thirdp/views.py
@@ -74,7 +74,6 @@ class dashboard(LoginRequiredMixin, generic.TemplateView):
         if '__book_ride_form_data' in request.session:
             kwargs['book_ride_form_data'] = request.session['__book_ride_form_data']
             print "Line 75", kwargs['book_ride_form_data']
-            kwargs['book_ride_form_data_email'] = kwargs['book_ride_form_data']['email']
             messages.success(request, "Payment Method has been saved!")
             #del request.session['__add_payment_revert']                
             
