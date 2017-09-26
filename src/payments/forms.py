7848ca1e4806f7159e8b65be50ea058bff5d493c
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-09-26 09:38:23 +0530

    UI Updates

--- src/payments/forms.py
+++ src/payments/forms.py
@@ -13,12 +13,13 @@ class paymentForm(forms.ModelForm):
         super(paymentForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_tag = False
+        self.fields["cvcCode"].widget.input_type = "password"  
         self.helper.layout = Layout(
             Field('nameOnCard', css_class="nameOnCard"),
             Field('cardNumber', css_class="cardNumber"),
             Field('expirationDateMM', css_class="expirationDateMM col-md-3"),
             Field('expirationDateYY', css_class="expirationDateYY col-md-4"),
-            Field('cvcCode',  widget=PasswordInput, css_class="cvcCode col-md-6"),
+            Field('cvcCode', css_class="cvcCode col-md-6"),
             Field('address', css_class="address"),
             Field('city', css_class="city col-md-6"),
             Field('state', css_class="state col-md-6"),
